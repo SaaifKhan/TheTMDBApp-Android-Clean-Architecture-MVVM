15854eef41dd82d6655271718f2bac32a9807495
saifkhan-sct <saif_khan09@hotmail.com>
2022-02-01 18:22:16 +0500

    View movies,View Artist and TvShows and update all three

--- app/src/main/java/com/saifkhan/mvvmcleanarch/thetmdpapp/presentation/splashActivity/SplashActivity.kt
+++ app/src/main/java/com/saifkhan/mvvmcleanarch/thetmdpapp/presentation/splashActivity/SplashActivity.kt
@@ -1,12 +1,21 @@
 package com.saifkhan.mvvmcleanarch.thetmdpapp.presentation.splashActivity
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
 import com.saifkhan.mvvmcleanarch.thetmdpapp.R
+import com.saifkhan.mvvmcleanarch.thetmdpapp.presentation.HomeActivity
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            startActivity(Intent(this@SplashActivity, HomeActivity::class.java))
+            finish()
+        }, 300)
     }
 }
